27cf10feffab9510a01667fca7ce44d5bc6763fa
exabugs <exabugs@gmail.com>
2017-09-18 13:07:03 +0900

    fix sagas/sessions.js

--- src/sagas/sessions.js
+++ src/sagas/sessions.js
@@ -14,15 +14,20 @@ export function* watchDoLogin(params) {
   } else {
     console.log(data);
     const { session } = data;
-    const result = {
-      userName: session.user.userName,
-      token: data.token,
-      token_type: data.token_type,
-      primaryGroup: session.group._id,
-    };
-    yield put(loginSuccess(result));
-    path = yield select(state => state.session.path);
-    console.log(path);
+
+    if (session && session.user) {
+      const result = {
+        userName: session.user.userName,
+        token: data.token,
+        token_type: data.token_type,
+        primaryGroup: session.group._id,
+      };
+      yield put(loginSuccess(result));
+      path = yield select(state => state.session.path);
+      console.log(path);
+    } else {
+      yield put(loginFailer(params));
+    }
   }
   yield browserHistory.push(path || '/');
 }
